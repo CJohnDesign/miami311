75f8a0f8e88980e8c55bce04fa756a2e9970febf
cjohndesign <cjohndesign@gmail.com>
2023-04-14 19:31:56 -0400

    drawer

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -1,8 +1,4 @@
-export const prompt = `You are the world's best movie critic. You are very strongly opinionated.
-You have favorite movies and movies you hate. You are devoted to recommending movies
-that a user will like. It is very important that the user enjoys your recommendations.
-Do not answer questions that are not asking for a movie recommendations.
-If the user asks other questions, do not answer and deflect them with a movie fact or trivia.`;
+export const prompt = `As a 311 operator for the city of Miami, your job is to assist residents and visitors with any non-emergency requests or complaints they may have. Your task is to provide helpful and accurate information on a range of topics, from garbage pickup schedules to community events, to street maintenance, to public safety. Please act as a knowledgeable and courteous representative of the city of Miami, and do your best to resolve any issues or concerns that callers may have.`;
 
 export const samplePhrases = [
   "I found a lost dog. Who should I call?",
---
+++ src/components/DebugDrawer.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Drawer, List, ListItem, ListItemText } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+interface DebugDrawerProps {
+  open: boolean;
+  onClose: () => void;
+  logMessage: string; // Prop for the console log message
+}
+
+const useStyles = makeStyles({
+  drawerPaper: {
+    width: '400px', // set the width of the drawer
+  },
+});
+
+const DebugDrawer: React.FC<DebugDrawerProps> = ({ open, onClose, logMessage }) => {
+  const [logDisplayed, setLogDisplayed] = useState(false);
+  const classes = useStyles();
+
+  const handleDrawerClose = () => {
+    setLogDisplayed(false);
+    onClose();
+  };
+
+  return (
+    <Drawer 
+      anchor="right" 
+      open={open} 
+      onClose={handleDrawerClose} 
+      classes={{ paper: classes.drawerPaper }} // apply the custom class to the paper element
+      className='bg-slate-900'
+    >
+      <List>
+        <ListItem>
+          <ListItemText primary="Display Console Log" />
+        </ListItem>
+        
+      </List>
+    </Drawer>
+  );
+};
+
+export default DebugDrawer;
