c1c0dcb30172e78adb7bd366fbd3428119f33d04
cjohndesign <cjohndesign@gmail.com>
2023-04-14 20:22:39 -0400

    drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
+import AppBar from '@material-ui/core/AppBar/index.js';
+import Toolbar from '@material-ui/core/Toolbar/index.js';
+import IconButton from '@material-ui/core/IconButton/index.js';
 import CodeIcon from '@material-ui/icons/Code';
 import DebugDrawer from './DebugDrawer';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles/index.js';
 
 const useStyles = makeStyles({
   appBar: {
